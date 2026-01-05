580aa36f2aa4e839ce8973b6f9833dc3a927c993
Gdonzeau <gdonzeau@hotmail.com>
2026-01-05 18:25:43 +0100

    Base

---
+++ MATT_URL/MATT_URL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MATT_URL
+//
+//  Created by Guillaume on 05/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MATT_URL/MATT_URL/MATT_URLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MATT_URLApp.swift
+//  MATT_URL
+//
+//  Created by Guillaume on 05/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MATT_URLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
